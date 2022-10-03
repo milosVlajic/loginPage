d3ead64d3d6848ae385351ec56bf94bb07ba5363
Milos Vlajic <mvlajic90@gmail.com>
2022-10-03 16:28:44 +0200

    start working on react router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import LoginPage from './components/LoginPage';
+import { Route } from 'react-router-dom';
 
 function App() {
   return (
--- src/index.js
+++ src/index.js
@@ -3,9 +3,15 @@ import { render } from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = document.getElementById('root');
-render(<App />, root);
+render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  root
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
