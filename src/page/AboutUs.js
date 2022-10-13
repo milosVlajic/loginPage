5a6b168372357b0eef32cc39ddceb168e10faedb
Milos Vlajic <mvlajic90@gmail.com>
2022-10-13 15:30:34 +0200

    fetching data work, start transform data for table

--- src/page/AboutUs.js
+++ src/page/AboutUs.js
@@ -1,8 +1,13 @@
 import { useQuery } from 'react-query';
 import React, { useState } from 'react';
+import { Button } from '@material-ui/core';
 
 const AboutUs = () => {
   const [table, setTable] = useState([]);
+
+  const fetchDataHandler = () => {
+    fetchTableHandler();
+  };
 
   function fetchTableHandler() {
     fetch('https://httpbin.org/get')
@@ -10,12 +15,18 @@ const AboutUs = () => {
         return response.json();
       })
       .then(data => {
-        setTable(data.results);
-        console.log(data.results);
+        setTable(data.headers);
+        console.log(data.headers);
       });
   }
-  fetchTableHandler();
-  return <div>AboutUs</div>;
+
+  return (
+    <div>
+      <Button color="primary" variant="outlined" onClick={fetchDataHandler}>
+        Fetch Table Data
+      </Button>
+    </div>
+  );
 };
 
 export default AboutUs;
