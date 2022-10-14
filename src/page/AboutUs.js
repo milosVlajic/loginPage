3a94be6b16e40b393fc03a4a522f2d376c10dc49
Milos Vlajic <mvlajic90@gmail.com>
2022-10-14 11:53:42 +0200

    start building table, do not work

--- src/page/AboutUs.js
+++ src/page/AboutUs.js
@@ -1,6 +1,16 @@
 import { useQuery } from 'react-query';
 import React, { useState } from 'react';
-import { Button } from '@material-ui/core';
+import {
+  Button,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+} from '@material-ui/core';
+import { makeStyles } from '@material-ui/styles';
 
 const AboutUs = () => {
   const [table, setTable] = useState([]);
@@ -8,6 +18,13 @@ const AboutUs = () => {
   const fetchDataHandler = () => {
     fetchTableHandler();
   };
+
+  const columns = [
+    { filed: 'name', headerName: 'Name' },
+    { field: 'value', headerName: 'Value' },
+  ];
+
+  let rows = [];
 
   function fetchTableHandler() {
     fetch('https://httpbin.org/get')
@@ -20,15 +37,27 @@ const AboutUs = () => {
         const tableObject = data.headers;
         for (const property in tableObject) {
           console.log(`${property} : ${tableObject[property]}`);
+          rows = [`${property}, ${tableObject[property]}`];
         }
       });
   }
+  console.log(rows);
 
   return (
     <div>
       <Button color="primary" variant="outlined" onClick={fetchDataHandler}>
         Fetch Table Data
       </Button>
+      <TableContainer component={Paper}>
+        <Table aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Name</TableCell>
+              <TableCell>Value</TableCell>
+            </TableRow>
+          </TableHead>
+        </Table>
+      </TableContainer>
     </div>
   );
 };
