fc755f83c5b46a5e0158ff8ea6296893ea98b982
Milos Vlajic <mvlajic90@gmail.com>
2022-10-18 13:37:52 +0200

    too many re-renders, still do not work

--- src/page/AboutUs.js
+++ src/page/AboutUs.js
@@ -14,86 +14,43 @@ import {
 import { makeStyles } from '@material-ui/styles';
 import Footer from '../components/Footer';
 
-
-
 const AboutUs = () => {
-
   const [table, setTable] = useState([]);
   const [rows, setRows] = useState([]);
- 
 
   const fetchTableData = () => {
-
-    fetch('https://httpbin.org/get').then(response => {
-      return response.json().then(data => {
-        setTable(data.headers);
-  
-          let tableArray = [];
-          const tableObject = data.headers;
-          for (const property in tableObject) {
-            tableArray.push(
-              createData(`${property}`, `${tableObject[property]}`)
-            );
-            console.log(tableArray);
-      }
-      setRows(tableArray)));
-    });
+    return fetch('https://httpbin.org/get').then(response => response.json());
   };
 
-     const createData = (name, value) => {
-     return { name, value };
-   };
+  const createData = (name, value) => {
+    return { name, value };
+  };
 
-   const columns = [
+  const columns = [
     { field: 'name', headerName: 'Name' },
     { field: 'value', headerName: 'Value' },
   ];
 
   const { isLoading, data } = useQuery('table-data', fetchTableData);
 
+  if (isLoading) {
+    return <span>Loading...</span>;
+  }
 
+  console.log(data.headers);
 
-  return (
-    <div>
-      {tableData || (
-        <Button color="primary" variant="outlined" onClick={fetchDataHandler}>
-          Fetch Table Data
-        </Button>
-      )}
-      {loading && <CircularProgress color="secondary" />}
+  let tableArray = [];
+  const tableObject = data.headers;
+  for (const property in tableObject) {
+    tableArray.push(createData(`${property}`, `${tableObject[property]}`));
+    console.log(tableArray);
+    // console.log(`${property} : ${tableObject[property]}`);
+  }
+  setRows(tableArray);
 
-      {tableData || <p>No items found</p>}
-
-      {tableData && (
-        <TableContainer component={Paper}>
-          <Table aria-label="simple table">
-            <TableHead>
-              <TableRow>
-                <TableCell>Name</TableCell>
-                <TableCell>Value</TableCell>
-              </TableRow>
-            </TableHead>
-
-            <TableBody>
-              {rows.map(row => (
-                <TableRow key={row.name}>
-                  <TableCell align="right">{row.name}</TableCell>
-                  <TableCell align="right">{row.value}</TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      )}
-      <Footer />
-    </div>
-  );
+  return <div>About Us</div>;
 };
 export default AboutUs;
-
-
-
-
 
 // const AboutUs = () => {
 //   const [table, setTable] = useState([]);
