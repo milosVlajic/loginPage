0d534f050a1261270246e4f617dc9d3fa7e0cacd
Milos Vlajic <mvlajic90@gmail.com>
2022-10-18 10:50:31 +0200

    start working on queries

--- src/page/AboutUs.js
+++ src/page/AboutUs.js
@@ -14,91 +14,110 @@ import {
 import { makeStyles } from '@material-ui/styles';
 import Footer from '../components/Footer';
 
+const fetchTableData = () => {
+  return fetch('https://httpbin.org/get');
+};
+
 const AboutUs = () => {
-  const [table, setTable] = useState([]);
-  const [rows, setRows] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [tableData, setTableData] = useState(false);
+  const { isLoading, data } = useQuery('table-data', fetchTableData);
 
-  // const rows = [
-  //   { name: 1, value: 3 },
-  //   { name: 2, value: 4 },
-  // ];
-
-  const fetchDataHandler = () => {
-    fetchTableHandler();
-  };
-
-  const createData = (name, value) => {
-    return { name, value };
-  };
-
-  const columns = [
-    { field: 'name', headerName: 'Name' },
-    { field: 'value', headerName: 'Value' },
-  ];
-
-  function fetchTableHandler() {
-    setLoading(true);
-    fetch('https://httpbin.org/get')
-      .then(response => {
-        return response.json();
-      })
-      .then(data => {
-        setTable(data.headers);
-
-        let tableArray = [];
-        const tableObject = data.headers;
-        for (const property in tableObject) {
-          tableArray.push(
-            createData(`${property}`, `${tableObject[property]}`)
-          );
-          console.log(tableArray);
-          // console.log(`${property} : ${tableObject[property]}`);
-        }
-        setRows(tableArray);
-        setLoading(false);
-        setTableData(true);
-      });
-  }
-
-  console.log(rows);
+  console.log(data);
 
   return (
     <div>
-      {tableData || (
-        <Button color="primary" variant="outlined" onClick={fetchDataHandler}>
-          Fetch Table Data
-        </Button>
-      )}
-      {loading && <CircularProgress color="secondary" />}
-
-      {tableData || <p>No items found</p>}
-
-      {tableData && (
-        <TableContainer component={Paper}>
-          <Table aria-label="simple table">
-            <TableHead>
-              <TableRow>
-                <TableCell>Name</TableCell>
-                <TableCell>Value</TableCell>
-              </TableRow>
-            </TableHead>
-
-            <TableBody>
-              {rows.map(row => (
-                <TableRow key={row.name}>
-                  <TableCell align="right">{row.name}</TableCell>
-                  <TableCell align="right">{row.value}</TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      )}
-      <Footer />
+      <Button color="primary" variant="outlined" onClick={fetchTableData}>
+        Fetch Table Data
+      </Button>
     </div>
   );
 };
-
 export default AboutUs;
+
+// const AboutUs = () => {
+//   const [table, setTable] = useState([]);
+//   const [rows, setRows] = useState([]);
+//   const [loading, setLoading] = useState(false);
+//   const [tableData, setTableData] = useState(false);
+
+//   // const rows = [
+//   //   { name: 1, value: 3 },
+//   //   { name: 2, value: 4 },
+//   // ];
+
+//   const fetchDataHandler = () => {
+//     fetchTableHandler();
+//   };
+
+//   const createData = (name, value) => {
+//     return { name, value };
+//   };
+
+//   const columns = [
+//     { field: 'name', headerName: 'Name' },
+//     { field: 'value', headerName: 'Value' },
+//   ];
+
+//   function fetchTableHandler() {
+//     setLoading(true);
+//     fetch('https://httpbin.org/get')
+//       .then(response => {
+//         return response.json();
+//       })
+//       .then(data => {
+//         setTable(data.headers);
+
+//         let tableArray = [];
+//         const tableObject = data.headers;
+//         for (const property in tableObject) {
+//           tableArray.push(
+//             createData(`${property}`, `${tableObject[property]}`)
+//           );
+//           console.log(tableArray);
+//           // console.log(`${property} : ${tableObject[property]}`);
+//         }
+//         setRows(tableArray);
+//         setLoading(false);
+//         setTableData(true);
+//       });
+//   }
+
+//   console.log(rows);
+
+//   return (
+//     <div>
+//       {tableData || (
+//         <Button color="primary" variant="outlined" onClick={fetchDataHandler}>
+//           Fetch Table Data
+//         </Button>
+//       )}
+//       {loading && <CircularProgress color="secondary" />}
+
+//       {tableData || <p>No items found</p>}
+
+//       {tableData && (
+//         <TableContainer component={Paper}>
+//           <Table aria-label="simple table">
+//             <TableHead>
+//               <TableRow>
+//                 <TableCell>Name</TableCell>
+//                 <TableCell>Value</TableCell>
+//               </TableRow>
+//             </TableHead>
+
+//             <TableBody>
+//               {rows.map(row => (
+//                 <TableRow key={row.name}>
+//                   <TableCell align="right">{row.name}</TableCell>
+//                   <TableCell align="right">{row.value}</TableCell>
+//                 </TableRow>
+//               ))}
+//             </TableBody>
+//           </Table>
+//         </TableContainer>
+//       )}
+//       <Footer />
+//     </div>
+//   );
+// };
+
+// export default AboutUs;
