0457dd5362230634311030aa26f2f6c87fb7f8bb
Milos Vlajic <mvlajic90@gmail.com>
2022-10-18 11:15:54 +0200

    working on query, still do not work

--- src/page/AboutUs.js
+++ src/page/AboutUs.js
@@ -14,24 +14,86 @@ import {
 import { makeStyles } from '@material-ui/styles';
 import Footer from '../components/Footer';
 
-const fetchTableData = () => {
-  return fetch('https://httpbin.org/get');
-};
+
 
 const AboutUs = () => {
+
+  const [table, setTable] = useState([]);
+  const [rows, setRows] = useState([]);
+ 
+
+  const fetchTableData = () => {
+
+    fetch('https://httpbin.org/get').then(response => {
+      return response.json().then(data => {
+        setTable(data.headers);
+  
+          let tableArray = [];
+          const tableObject = data.headers;
+          for (const property in tableObject) {
+            tableArray.push(
+              createData(`${property}`, `${tableObject[property]}`)
+            );
+            console.log(tableArray);
+      }
+      setRows(tableArray)));
+    });
+  };
+
+     const createData = (name, value) => {
+     return { name, value };
+   };
+
+   const columns = [
+    { field: 'name', headerName: 'Name' },
+    { field: 'value', headerName: 'Value' },
+  ];
+
   const { isLoading, data } = useQuery('table-data', fetchTableData);
 
-  console.log(data);
+
 
   return (
     <div>
-      <Button color="primary" variant="outlined" onClick={fetchTableData}>
-        Fetch Table Data
-      </Button>
+      {tableData || (
+        <Button color="primary" variant="outlined" onClick={fetchDataHandler}>
+          Fetch Table Data
+        </Button>
+      )}
+      {loading && <CircularProgress color="secondary" />}
+
+      {tableData || <p>No items found</p>}
+
+      {tableData && (
+        <TableContainer component={Paper}>
+          <Table aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Name</TableCell>
+                <TableCell>Value</TableCell>
+              </TableRow>
+            </TableHead>
+
+            <TableBody>
+              {rows.map(row => (
+                <TableRow key={row.name}>
+                  <TableCell align="right">{row.name}</TableCell>
+                  <TableCell align="right">{row.value}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      )}
+      <Footer />
     </div>
   );
 };
 export default AboutUs;
+
+
+
+
 
 // const AboutUs = () => {
 //   const [table, setTable] = useState([]);
